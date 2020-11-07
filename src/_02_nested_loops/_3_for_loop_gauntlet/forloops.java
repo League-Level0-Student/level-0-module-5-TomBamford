875ec502cf5cc8eaf47cf7f274496feefb56d0a5
root <root@8ea22f21703b>
2020-11-07 22:24:49 +0000

    Week 10 or something

--- src/_02_nested_loops/_3_for_loop_gauntlet/forloops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/forloops.java
@@ -2,10 +2,10 @@ package _02_nested_loops._3_for_loop_gauntlet;
 
 public class forloops {
 	public static void main(String[] args) {
-		for ( int i = 0; i < 100; i++) {
+		for ( int i = 0; i < 101; i++) {
 			System.out.println(i);
 		}
-		for (int i = 101; i > 0; i--) {
+		for (int i = 100; i > 0; i--) {
 			System.out.println(i);
 		}
 		for (int i = 2; i < 100; i++) {
@@ -22,10 +22,10 @@ public class forloops {
 		}
 		for ( int i = 1; i < 500; i++) {
 			if ( i % 2 ==0) {
-				System.out.println(i + "is even");
+				System.out.println(i + " " + "is even");
 			}
 			else {
-				System.out.println(i + "is odd");
+				System.out.println(i + " " + "is odd");
 			}
 		}
 		for (int i = 7; i < 777; i++) {
@@ -41,5 +41,34 @@ public class forloops {
 		for(int i = 0; i < 3; i++) {
 			
 		}
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println(i + " " + j);
+			}
+			}
+		int n = 1; 
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+					System.out.print(n + " ");
+					n = n+1;
+			}
+			System.out.println();
+			}
+		int k = 1;
+		for( int i = 0; i < 10; i ++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.print(k + " ");
+				k = k+1;
+			}
+			System.out.println();
+		}
+		String v = "*";
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print(v);
+			}
+			System.out.println();
+		}
+		}
 	}
-}
+
